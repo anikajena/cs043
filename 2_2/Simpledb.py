f943243013dbf205d89994e623161d6692af7535
anikajena <anikajena@yahoo.com>
2021-04-06 10:35:41 -0700

    Initial commit

---
+++ 2_2/Simpledb.py
@@ -0,0 +1,12 @@
+import self as self
+
+
+
+class Simpledb:
+    db = Simpledb('phonenumbers.txt')
+    db.insert(self, 'Anika Jena', '1234567890')
+    db.insert(self, 'Beatriz Olga', '2345678901')
+    db.insert(self, 'Casper Kang', '3456789012')
+    db.insert(self, 'Dev Saxena', '4567890123')
+    db.insert(self, 'Eunji Lee', '5678901234')
+    db.insert(self, 'Fernan Mayri', '6789012345')
